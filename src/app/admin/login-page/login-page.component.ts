a41673be25942cd6c94ecc3361fee899846c4c5a
Tata Rozhdez <tanya.zhukova.95@gmail.com>
2021-01-08 17:41:01 +0200

    admin router

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core'
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 import {User} from '../../shared/interfaces';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -10,9 +12,12 @@ import {User} from '../../shared/interfaces';
 export class LoginPageComponent implements OnInit {
 
   form: FormGroup
+  submitted = false
 
-  constructor() {
-  }
+  constructor(
+    public auth: AuthService,
+    private router: Router 
+  ) { }
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -32,11 +37,20 @@ export class LoginPageComponent implements OnInit {
       return
     }
 
+    this.submitted = true
+
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password
     }
 
+    this.auth.login(user).subscribe(() => {
+      this.form.reset()
+      this.router.navigate(['/admin', 'dashboard'])
+      this.submitted = false
+    }, () => {
+        this.submitted = false
+    })
 
   }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyDEzHNBWS5r4Lif2nTMu77csZQPrga_VL8'
+};
