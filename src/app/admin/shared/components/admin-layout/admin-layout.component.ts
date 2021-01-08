8d4ae9e8355652850527a13d3ae0d699e0741e5c
Tata Rozhdez <tanya.zhukova.95@gmail.com>
2021-01-08 15:05:56 +0200

    admin layout page

--- src/app/admin/shared/components/admin-layout/admin-layout.component.ts
+++ src/app/admin/shared/components/admin-layout/admin-layout.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-layout',
@@ -8,14 +7,9 @@ import { Router } from '@angular/router';
 })
 export class AdminLayoutComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  logout(event: Event) {
-    event.preventDefault()
-    this.router.navigate(['/admin', 'login'])
   }
 
 }
